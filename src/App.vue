1a792785f7b6707b4ceb26942122f62e51e46cb7
unhurried <unhurried@users.noreply.github.com>
2019-02-07 23:26:36 +0900

    Support build for a library.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import axios from 'axios';
 import marked from 'marked';
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus, faMinus, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+library.add(faPlus, faMinus, faExternalLinkAlt);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+import TreeList from '@/components/TreeList.vue';
+Vue.component('TreeList', TreeList);
+
 @Component
 export default class App extends Vue {
   private doc: Element | null = null;
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-Vue.use(BootstrapVue);
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlus, faMinus, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-library.add(faPlus, faMinus, faExternalLinkAlt);
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-import TreeList from '@/components/TreeList.vue';
-Vue.component('TreeList', TreeList);
-
 Vue.config.productionTip = false;
 
 new Vue({
