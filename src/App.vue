2e8009fb816f77d57b72bf99509077e9474d0b92
unhurried <unhurried@users.noreply.github.com>
2019-03-02 22:20:06 +0900

    Add file-import function.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <ImportButton class="float-right" @fileLoaded="onFileLoaded" />
     <TreeList :doc="doc"/>
   </div>
 </template>
@@ -15,24 +16,37 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(BootstrapVue);
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlus, faMinus, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-library.add(faPlus, faMinus, faExternalLinkAlt);
+import { faPlus, faMinus, faExternalLinkAlt, faFileImport } from '@fortawesome/free-solid-svg-icons';
+library.add(faPlus, faMinus, faExternalLinkAlt, faFileImport);
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
+// Recursive components need to be registered globally.
 import TreeList from '@/components/TreeList.vue';
 Vue.component('TreeList', TreeList);
 
-@Component
+import ImportButton from '@/components/ImportButton.vue';
+
+@Component({
+  components: {
+    ImportButton,
+  },
+})
 export default class App extends Vue {
   @Prop({ default: 'tree.md' }) private path!: string;
   private doc: Element | null = null;
-  public created() {
+  private created() {
     axios.get(this.path).then((res) => {
-      const html = marked(res.data);
-      const parsedHtml = new DOMParser().parseFromString(html, 'text/html');
-      this.doc = parsedHtml.querySelector('html body ul');
+      this.updateDoc(res.data);
     });
+  }
+  private onFileLoaded(content: string): void {
+    this.updateDoc(content);
+  }
+  private updateDoc(newDoc: string) {
+    const html = marked(newDoc);
+    const parsedHtml = new DOMParser().parseFromString(html, 'text/html');
+    this.doc = parsedHtml.querySelector('html body ul');
   }
 }
 </script>
